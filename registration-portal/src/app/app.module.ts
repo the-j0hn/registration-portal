5a12a1598c430614804607f1f03626f5b51d25a1
thejohn <johnvarghese982@gmail.com>
2020-04-15 00:05:52 +0530

    US00000: Screen 1 UI changes

--- registration-portal/src/app/app.module.ts
+++ registration-portal/src/app/app.module.ts
@@ -2,13 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from 'src/home/home.component';
+import { RegistrationComponent } from 'src/registration/registration.compnent';
+import { ProfileComponent } from 'src/profile/profile.component';
+import { RouterModule } from '@angular/router';
+import { RouteModule } from 'src/routing/route.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RegistrationComponent,
+    ProfileComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouteModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ registration-portal/src/home/home.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'rp-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit{
+  
+  constructor(
+    private route: Router
+  ){}
+
+  public ngOnInit(): void {
+
+  }
+
+  public register(): void {
+    this.route.navigate(['/profile']);
+  }
+}
